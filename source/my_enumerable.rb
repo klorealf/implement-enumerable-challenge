494f3a78031015e8e47e79208bba9c90cf123d73
Torey Hickman <torey@toreyhickman.com>
2015-05-07 20:13:18 -0500

    Move my_all? and my_reduce to the bottom of the class

--- source/my_enumerable.rb
+++ source/my_enumerable.rb
@@ -1,19 +1,25 @@
 module MyEnumerable
-  def my_all? #optional
-  end
-
   def my_find
   end
 
   def my_map
   end
 
-  def my_reduce #optional
-  end
-
   def my_reject
   end
 
   def my_select
+  end
+
+  # Coding #my_all? is optional.
+  def my_all?
+  end
+
+  # Coding #my_reduce is optional.
+  # You may change the number of arguments
+  # that the method accepts, if you choose,
+  # but the method will need to accept
+  # 0 - 2 arguments.
+  def my_reduce(arg_1 = nil, arg_2 = nil)
   end
 end
