91519273b373bc548e97ec4c5459f419f80d74df
Thomas Rafferty & bradsloan <pair+ThomasRafferty.bsloan3@devbootcamp.com>
2017-08-29 16:02:11 -0400

    write select method

--- enumerating_methods.rb
+++ enumerating_methods.rb
@@ -40,6 +40,15 @@ def reject(array)
 end
 
 def select(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    if yield(array[i]) == true
+      new_array << array[i]
+    end
+    i += 1
+  end
+  new_array
 end
 
 # Coding all? is optional.
